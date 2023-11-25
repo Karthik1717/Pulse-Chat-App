0c5d5706757587d1e5f109c6dfa1364e0f00791e
Karthik1717 <karthikkamath98@yahoo.com>
2023-11-26 01:08:08 +0530

    Added Authentication and Login logic

--- backend/server.js
+++ backend/server.js
@@ -1,23 +1,25 @@
 const express = require("express");
 const dotenv = require("dotenv");
-
 const chats = require("./data/data");
+const connectDB = require("./config/db");
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+const { errorHandler, notFound } = require("./middleware/errorMiddleware");
 
+dotenv.config({ path: __dirname + `/.env` });
+connectDB();
 const app = express();
-dotenv.config();
+
+app.use(express.json());
 
 app.get("/", (req, res) => {
 	res.send("API is Running");
 });
 
-app.get("/api/chat", (req, res) => {
-	res.send(chats);
-});
+app.use("/api/user", userRoutes);
 
-app.get("/api/chat/:id", (req, res) => {
-	const singleChat = chats.find((c) => (c.id = req.params.id));
-	res.send(singleChat);
-});
+app.use(notFound);
+app.use(errorHandler);
 
 const PORT = process.env.PORT || 5000;
-app.listen(5000, console.log(`Server Started on PORT ${PORT}`));
+app.listen(5000, console.log(`Server Started on PORT ${PORT}`.yellow.bold));
