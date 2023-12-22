b6a52e2c44dfd8f7a8e704fb11dcf74a47d44141
Karthik1717 <karthikkamath98@yahoo.com>
2023-12-22 15:41:37 +0530

    Aded chat styles

--- src/App.js
+++ src/App.js
@@ -1,25 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import { Button } from "@chakra-ui/react";
+import "./App.css";
+import { Route } from "react-router-dom";
+import Homepage from "./Pages/Homepage";
+import Chats from "./Pages/ChatPage";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<div className='App'>
+			<Route path='/' component={Homepage} exact />
+			<Route path='/chats' component={Chats} />
+		</div>
+	);
 }
 
 export default App;
